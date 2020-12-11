d6448dfef2e1e8cbf7999afaa2eb16c27a66f738
DovYoung <1591759055@qq.com>
2020-12-11 11:58:29 +0800

    FizzBuzz

--- LeetCodeDemo/LeetCodeDemo/main.swift
+++ LeetCodeDemo/LeetCodeDemo/main.swift
@@ -9,12 +9,18 @@ import Foundation
 
 print("Hello, World!")
 
-let t01 = TwoSum()
-t01.test()
+//let t01 = TwoSum()
+//t01.test()
+//
+//let t02 = Intersect()
+//t02.test()
+//
+//let t03 = SlidingWindowMaximum()
+//t03.test()
 
-let t02 = Intersect()
-t02.test()
+let dic = [3: "Fizz", 5: "Buzz"]
 
-let t03 = SlidingWindowMaximum()
-t03.test()
-
+//let dic = ["a" : 1, "b" : 2, "c" : 3, "d" : 4]
+for key in dic.keys.sorted(by: <) {
+    print("key: \(key), value: \(dic[key] ?? "-1")")
+}
