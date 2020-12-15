f8eab48c7a507ada9a9b0f75d8a112b6fb9b2348
DovYoung <1591759055@qq.com>
2020-12-15 20:55:25 +0800

    文件整理

---
+++ LeetCodeDemo/LeetCodeDemo/Week03/GenerateParenthesis.swift
@@ -0,0 +1,15 @@
+//
+//  GenerateParenthesis.swift
+//  LeetCodeDemo
+//
+//  Created by Yangdongwu on 2020/12/15.
+//
+
+import Foundation
+
+class GenerateParenthesis {
+    
+    func generateParenthesis(_ n: Int) -> [String] {
+        return []
+    }
+}
