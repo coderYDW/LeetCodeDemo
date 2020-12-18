3718e9b3b9d6aa0b9f082851a085c7a149b12fed
DovYoung <1591759055@qq.com>
2020-12-18 11:07:23 +0800

    做题
    括号生成
    翻转二叉树

--- LeetCodeDemo/LeetCodeDemo/Week03/GenerateParenthesis.swift
+++ LeetCodeDemo/LeetCodeDemo/Week03/GenerateParenthesis.swift
@@ -10,6 +10,23 @@ import Foundation
 class GenerateParenthesis {
     
     func generateParenthesis(_ n: Int) -> [String] {
-        return []
+        var res = [String]()
+        generate(0, 0, n, "", &res)
+        return res
     }
+    
+    func generate(_ left: Int, _ right: Int, _ n: Int, _ s: String, _ res: inout [String]) {
+        if left == n && right == n {
+            res.append(s)
+            return
+        }
+        if left < n {
+            generate(left + 1, right, n, s + "(", &res)
+        }
+        if left > right {        
+            generate(left, right + 1, n, s + ")", &res)
+        }
+    }
+    
+    
 }
