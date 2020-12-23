d444e84124572cdcd6b435a92007efe2a6760b0a
杨冬武 <coderYDW@users.noreply.github.com>
2020-12-23 11:59:28 +0800

    电话号码的字母组合优化

--- LeetCodeDemo/LeetCodeDemo/Week03/LetterOfPhoneNumber.swift
+++ LeetCodeDemo/LeetCodeDemo/Week03/LetterOfPhoneNumber.swift
@@ -8,22 +8,24 @@
 import Foundation
 
 class LetterOfPhoneNumber {
-    /* 电话号码的字母组合 */
+    /*
+     电话号码的字母组合
+     ["2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"]
+     */
     func letterCombinations(_ digits: String) -> [String] {
         guard digits.count > 0 else {
             return []
         }
-        var res = [String]()
-        var temp = ""
+        var res = [String](), temp = ""
         let dic: [Character: String] = ["2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"]
         func backtrack(_ index: Int) {
             if index == digits.count {
-                res.append(String(temp))
+                res.append(temp)
                 return
             }
             let str = dic[digits[digits.index(digits.startIndex, offsetBy: index)]]
             for c in str! {
-                temp.append(String(c))
+                temp.append(c)
                 backtrack(index + 1)
                 temp.removeLast()
             }
