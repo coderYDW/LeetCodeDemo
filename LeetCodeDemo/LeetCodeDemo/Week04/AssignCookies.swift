564590450424322becddc1da7083c12af5238bd0
DovYoung <1591759055@qq.com>
2020-12-24 22:56:07 +0800

    注释

--- LeetCodeDemo/LeetCodeDemo/Week04/AssignCookies.swift
+++ LeetCodeDemo/LeetCodeDemo/Week04/AssignCookies.swift
@@ -12,12 +12,12 @@ class AssignCookies {
     func findContentChildren(_ g: [Int], _ s: [Int]) -> Int {
         let gSorted = g.sorted() // 孩子胃口排序
         let sSorted = s.sorted() // 饼干排序
-        var res = 0, sIndex = 0
+        var res = 0, sIndex = 0  // 最终结果和饼干的下标
         while sIndex < s.count && res < g.count {
             if sSorted[sIndex] >= gSorted[res]  {
-                res += 1
+                res += 1 // 如果饼干满足孩子,结果加一
             }
-            sIndex += 1
+            sIndex += 1 // 无论饼干是否满足,结果都加一
         }
         return res
     }
