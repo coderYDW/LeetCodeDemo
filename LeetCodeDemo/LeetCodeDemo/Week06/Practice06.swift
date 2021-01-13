d788eb92cb8704bd50e52419e2b06eaa14a47de5
DovYoung <1591759055@qq.com>
2021-01-13 23:01:31 +0800

    回文子串练习

--- LeetCodeDemo/LeetCodeDemo/Week06/Practice06.swift
+++ LeetCodeDemo/LeetCodeDemo/Week06/Practice06.swift
@@ -156,4 +156,43 @@ class Practice06 {
         return cur
     }
     
+    func countSubstrings(_ s: String) -> Int {
+        var t = Array("$#")
+        let s = Array(s)
+        for c in s {
+            t.append(c)
+            t.append("#")
+        }
+        let n = t.count
+        t.append("!")
+        var rMax = 0, iMax = 0, ans = 0
+        var f = [Int](repeating: 0, count: n)
+        for i in 1..<n {
+            f[i] = i <= rMax ? min(rMax - i + 1, f[2 * iMax - i]) : 1
+            while t[i + f[i]] == t[i - f[i]] {
+                f[i] += 1
+            }
+            if i + f[i] - 1 > rMax {
+                iMax = i
+                rMax = i + f[i] - 1
+            }
+            ans += f[i] / 2
+        }
+        return ans
+    }
+    
+    func countSubstrings10(_ s: String) -> Int {
+        let s = Array(s), n = s.count
+        var ans = 0
+        for i in 0..<2 * n - 1 {
+            var l = i / 2, r = i / 2 + i % 2
+            while l >= 0 && r < n && s[l] == s[r] {
+                ans += 1
+                l -= 1
+                r += 1
+            }
+        }
+        return ans
+    }
+    
 }
