325c571626f4dbe9fc8536c7a2a951d096875144
DovYoung <1591759055@qq.com>
2021-01-17 22:14:04 +0800

    Update ClimbStairs.swift

--- LeetCodeDemo/LeetCodeDemo/Week03/ClimbStairs.swift
+++ LeetCodeDemo/LeetCodeDemo/Week03/ClimbStairs.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class ClimbStairs {
-    
+    //动态规划
     func climbStairs(_ n: Int) -> Int {
         var p = 0, q = 0, r = 1
         for _ in 0..<n {
@@ -26,5 +26,19 @@ class ClimbStairs {
             dic[n] = climbStairs(n - 1) + climbStairs(n - 2)
         }
         return dic[n]!
+    }
+    //可以指定每步台阶数
+    func climbStairs20(_ n: Int) -> Int {
+        let steps = [1, 2]
+        var dp = [Int](repeating: 0, count: n + 1)
+        dp[0] = 1
+        for i in 0..<n + 1 {
+            for step in steps {
+                if step <= i {
+                    dp[i] += dp[i - step]
+                }
+            }
+        }
+        return dp[n]
     }
 }
