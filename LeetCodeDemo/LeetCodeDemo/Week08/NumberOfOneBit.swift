bfc79f7391a9a24a97047159f799a14280f03235
DovYoung <1591759055@qq.com>
2021-01-21 14:22:03 +0800

    位1的个数

---
+++ LeetCodeDemo/LeetCodeDemo/Week08/NumberOfOneBit.swift
@@ -0,0 +1,19 @@
+//
+//  NumberOfOneBit.swift
+//  LeetCodeDemo
+//
+//  Created by Yangdongwu on 2021/1/21.
+//
+
+import Foundation
+
+class NumberOfOneBit {
+    func hammingWeight(_ n: Int) -> Int {
+        var res = 0, n = n
+        while n != 0 {
+            n &= (n - 1)
+            res += 1
+        }
+        return res
+    }
+}
