0ecd58fc1b609e884cfb546de0a5ee14f8f19863
杨冬武 <coderYDW@users.noreply.github.com>
2021-01-25 10:55:02 +0800

    插入排序

---
+++ LeetCodeDemo/LeetCodeDemo/Week08/Sort.swift
@@ -0,0 +1,43 @@
+//
+//  Sort.swift
+//  LeetCodeDemo
+//
+//  Created by Apple on 2021/1/25.
+//
+
+import Foundation
+
+class Sort {
+    
+    /*
+     插入排序
+     步骤如下：
+     1. 从第一个元素开始，该元素可以认为已经被排序
+     2. 取出下一个元素，在已经排序的元素序列中从后向前扫描
+     3. 如果该元素（已排序）大于新元素，将该元素移到下一位置
+     4. 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+     5. 将新元素插入到该位置中
+     6. 重复步骤2
+     */
+    func insertSort(_ arr: inout [Int]) {
+        for i in 1..<arr.count {
+            let key = arr[i]
+            var j = i - 1
+            while j >= 0 && arr[j] > key {
+                arr[j + 1] = arr[j]
+                j -= 1
+            }
+            arr[j + 1] = key
+        }
+    }
+    
+    
+    
+    
+    func test() {
+        var arr = [15,2,1,6,7,9,34,5]
+        insertSort(&arr)
+        print(arr)
+    }
+    
+}
--- LeetCodeDemo/LeetCodeDemo/main.swift
+++ LeetCodeDemo/LeetCodeDemo/main.swift
@@ -41,5 +41,5 @@ print("Hello, World!")
 //let b = a[2...]
 //print(b)
 
-let addd = SolveSudoku.init()
+let addd = Sort()
 addd.test()
