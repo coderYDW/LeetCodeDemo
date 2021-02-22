d7d2569a35d56924b55a44997a1ba7c300f07281
杨冬武 <coderYDW@users.noreply.github.com>
2021-02-22 13:19:02 +0800

    链表

---
+++ LeetCodeDemo/LeetCodeDemo/LinkList/LinkList.swift
@@ -0,0 +1,27 @@
+//
+//  LinkList.swift
+//  LeetCodeDemo
+//
+//  Created by Apple on 2021/2/22.
+//
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    
+    public init() {
+        self.val = 0
+        self.next = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class LinkList {
+    
+}
--- LeetCodeDemo/LeetCodeDemo/Week03/ReversePrint.swift
+++ LeetCodeDemo/LeetCodeDemo/Week03/ReversePrint.swift
@@ -76,11 +76,3 @@ class ReversePrint {
     }
 }
 
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
-    }
-}
