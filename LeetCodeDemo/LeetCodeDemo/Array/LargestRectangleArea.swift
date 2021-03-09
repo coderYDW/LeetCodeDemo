aba60f09a3797be6432a0034b4807fbb21881b20
杨冬武 <coderYDW@users.noreply.github.com>
2021-03-09 11:36:33 +0800

    柱状图的最大矩形

--- LeetCodeDemo/LeetCodeDemo/Array/LargestRectangleArea.swift
+++ LeetCodeDemo/LeetCodeDemo/Array/LargestRectangleArea.swift
@@ -14,7 +14,32 @@ import Foundation
  */
 
 class LargestRectangleArea {
+    
     func largestRectangleArea(_ heights: [Int]) -> Int {
+        var heights = heights
+        //添加一个元素0，作为哨兵，因为高度都大于等于0，都会进行计算面积
+        heights.append(0)
+        var stack = [Int]()
+        var res = 0
+        for i in heights.indices {
+            //这里的heights[stack.last!] >= heights[i]大于或者大于等于都可以
+            //两个数相等，左右边界都是一样的
+            //如果栈顶的值大于新入栈的值，需要计算面积高度并移除栈顶元素，因为已经找到了右边界
+            while !stack.isEmpty && heights[stack.last!] >= heights[i] {
+                //计算面积
+                let h = heights[stack.removeLast()]
+                let w = i - (stack.last ?? -1) - 1
+                res = max(res, w * h)
+            }
+            //否则将 i 入栈
+            stack.append(i)
+        }
+        //移除临时添加的0
+        heights.removeLast()
+        return res
+    }
+    
+    func largestRectangleArea10(_ heights: [Int]) -> Int {
         let n = heights.count
         var left = [Int](repeating: 0, count: n), right = [Int](repeating: n, count: n)
         var stack = [Int]()
@@ -33,19 +58,34 @@ class LargestRectangleArea {
         return ans
     }
     
-    func largestRectangleArea10(_ heights: [Int]) -> Int {
-        var heights = heights
-        heights.append(0)
-        var stack = [Int]()
-        var ans = 0
-        for i in heights.indices {
-            while !stack.isEmpty && heights[stack.last!] >= heights[i] {
-                let height = heights[stack.removeLast()]
-                let length = i - (stack.last ?? -1) - 1
-                ans = max(ans, height * length)
-            }
-            stack.append(i)
+    func largestRectangleArea20(_ heights: [Int]) -> Int {
+        let n = heights.count
+        guard n > 0 else {
+            return 0
         }
-        return ans
+        var lessFromLeft = [Int](repeating: 0, count: n)
+        var lessFromRight = [Int](repeating: 0, count: n)
+        lessFromLeft[0] = -1
+        lessFromRight[n - 1] = n
+        for i in 1..<n {
+            var p = i - 1
+            while p >= 0 && heights[p] >= heights[i] {
+                p = lessFromLeft[p]
+            }
+            lessFromLeft[i] = p
+        }
+        for j in (0..<n - 1).reversed() {
+            var p = j + 1
+            while p < n && heights[p] >= heights[j] {
+                p = lessFromRight[p]
+            }
+            lessFromRight[j] = p
+        }
+        var maxVal = 0
+        for i in 0..<n {
+            maxVal = max(maxVal, (lessFromRight[i] - lessFromLeft[i] - 1) * heights[i])
+        }
+        return maxVal
     }
+    
 }
